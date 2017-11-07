b692c398583fcf4c15a4deffd3cfca51557e3df1
Csaba Kigyosi <csabronc@gmail.com>
2017-11-07 21:31:41 +0100

    add projects

---
+++ src/app/components/addproject/addproject.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addproject',
+  templateUrl: './addproject.component.html',
+  styleUrls: ['./addproject.component.css']
+})
+export class AddProjectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    console.log('why isnt it owrking man?');
+  }
+}
+export class Task {
+  id: number;
+  name: string;
+  plannedHours: number;
+  maxHours: number;
+  requiredSkills: string;
+  startDate: string;
+  dueDate: string;
+  projectLeader: string;
+}
---
+++ src/app/models/Project.ts
@@ -0,0 +1,11 @@
+export interface Project {
+  id: number;
+  name: string;
+  company: string;
+  email: string;
+  phone: string;
+  workinghours: number;
+  salary: number;
+  skills: string;
+  hide: boolean;
+}
