e76218596841968fcfec318a610753079e493936
Csaba Kigyosi <csabronc@gmail.com>
2017-11-08 01:38:57 +0100

    add mock file

---
+++ src/app/mockdata/mock-resources.ts
@@ -0,0 +1,46 @@
+import { Resource } from '../models/Resource';
+
+export const RESOURCES: Resource[] = [
+  {
+    id: 1,
+    firstname: 'Csaba',
+    lastname: 'Kigyosi',
+    email: 'asdad@gmail.com',
+    salary: 2000,
+    company: 'BME',
+    phone: 2222,
+    availablefrom: 2200,
+    availableto: 2300,
+    workinghours: 8,
+    skills: 'JavaScript',
+    hide: false
+  },
+  {
+    id: 1,
+    firstname: 'Jolan',
+    lastname: 'Megyei',
+    email: 'asdad@gmail.com',
+    salary: 100,
+    company: 'UFO',
+    phone: 233,
+    availablefrom: 20000,
+    availableto: 200,
+    workinghours: 6,
+    skills: 'Nothing Really',
+    hide: false
+  },
+  {
+    id: 1,
+    firstname: 'Jancsika',
+    lastname: 'Kiss',
+    email: 'asdad@gmail.com',
+    salary: 2000,
+    company: 'ELTE',
+    phone: 6666,
+    availablefrom: 7200,
+    availableto: 6300,
+    workinghours: 2,
+    skills: 'C++',
+    hide: false
+  }
+];
